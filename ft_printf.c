ff05a69101d53ed1d014d114f5a52fd39a8956a9
Redwyn Poetess <rpoetess@il-k1.21-school.ru>
2019-08-25 22:45:14 +0300

    2

--- ft_printf.c
+++ ft_printf.c
@@ -21,29 +21,29 @@ int     check_flags(char format)
     return(0);
 }
 
-var*    ft_controller(var *tmp)
+char*    ft_controller(var *tmp)
 {
-    while(1)
-    {
-        if (tmp->type == '%')
-            ft_print_pc(tmp);
-        else if (tmp->type == 'c')
-            ft_print_c(tmp);
-        else if (tmp->type == 's')
-            ft_print_s(tmp);
-        else if (tmp->type == 'p')
-            ft_print_p(tmp);
-        else if (tmp->type == 'd' || tmp->type == 'i')
-            ft_print_d(tmp);
-        else if (tmp->type == 'o')
-            ft_print_o(tmp);
-        else if (tmp->type == 'u' || tmp->type == 'U')
-            ft_print_u(tmp);
-        else if (tmp->type == 'x' || tmp->type == 'X')
-            ft_print_X(tmp);
-        if (tmp->type == 'f')
-            ft_print_f(tmp);
-    }
+    char *res;
+
+    if (tmp->type == '%')
+        res = ft_print_pc(tmp);
+    else if (tmp->type == 'c')
+        res = ft_print_c(tmp);
+    else if (tmp->type == 's')
+        res = ft_print_s(tmp);
+    else if (tmp->type == 'p')
+        res = ft_print_p(tmp);
+    else if (tmp->type == 'd' || tmp->type == 'i')
+        res = ft_print_d(tmp);
+    else if (tmp->type == 'o')
+        res = ft_print_o(tmp);
+    else if (tmp->type == 'u' || tmp->type == 'U')
+        res = ft_print_u(tmp);
+    else if (tmp->type == 'x' || tmp->type == 'X')
+        res = ft_print_X(tmp);
+    if (tmp->type == 'f')
+        res = ft_print_f(tmp);
+    return (res);
 }
 
 /*void    print_result(result_list *res)
@@ -51,11 +51,12 @@ var*    ft_controller(var *tmp)
     while()
 }*/
 
-var*        create_list_var(const char *mas, int *i, va_list str, result_list* list)
+char*        create_list_var(const char *mas, int *i, va_list str, var* list)
 {
     char    *str1;
- 
+     write(1, "2.0\n", 4);
     str1 = va_arg(str, char*);
+    write(1, "2.1\n", 4);
     var *tmp = (var*)malloc(sizeof(var));
     if ((mas[*i] == '+' || mas[*i] == '-' || mas[*i] == '+' || 
         mas[*i] == '#' || mas[*i] == '0') && !tmp->flags)
@@ -67,8 +68,10 @@ var*        create_list_var(const char *mas, int *i, va_list str, result_list* l
     tmp->precision = ft_strcat(tmp->precision, &mas[*i++]);
     if (check_flags(mas[*i]))
         exit(0);
+    write(1, "2.2\n", 4);
     tmp->type = mas[*i];
     tmp->data = str1;
+    write(1, "2.3\n", 4);
     return(ft_controller(tmp));
 }
 
@@ -103,11 +106,14 @@ int         ft_printf(const char *format, ...)
 	va_start(str, format);
     while (format[i] != '\0')
     {
+        res_tmp = (result_list*)malloc(sizeof(result_list));
+        res_str->next = res_tmp;
+        res_str = res_tmp;
+        
         while (format[i] != '%' && format[i] != '\0')
             {
-                    write(1, "1.0\n", 4);
                 res_str->data = ft_strnew(1);
-                    write(1, "1.1\n", 4);
+                /*    write(1, "1.1\n", 4);
                 res_str->data[0] = format[i++];
                     write(1, "1.2\n", 4);
                 res_tmp = res_str;
@@ -115,14 +121,19 @@ int         ft_printf(const char *format, ...)
                 res_str = create_list_result();
                     write(1, "1.3\n", 4);
                 res_str = res_tmp->next;
-                    write(1, "1.3\n", 4);
+                    write(1, "1.3\n", 4);*/
+                res_str->data = ft_strjoin(res_str->data, &format[i++]);
             }
-            write(1, "2\n", 2);
         if (format[i] == '%')
             {
                 ++i;
-                res_str->data = create_list_var(format, &i, str, res_str)->data;
-                res_str = res_str->next;
+                var_tmp = (var*)malloc(sizeof(var_tmp));
+                write(1, "1.0\n", 4);
+                var_tmp->data = create_list_var(format, &i, str, var_tmp);
+                write(1, "1.1\n", 4);
+                res_str->data = var_tmp->data;
+                write(1, "1.2\n", 4);
+                //free_var(var_tmp)
             }
         i++;
     }
