0f5e4fa5ca0a14d56814c2142c7cb856abe93066
Corvettus <mackar.danilov@yandex.ru>
2019-08-26 17:22:56 +0300

    pp

--- ft_print_p.c
+++ ft_print_p.c
@@ -1,48 +1,126 @@
 #include "ft_printf.h"
 
+void	ft_supzerox(t_spec *s, t_ran *ran)
+{
+	str[i++] = '0';
+	str[i++] = 'x';
+	if (s->precision > s->width)
+		while (s->precision-- > ft_strlen(s))
+			str[i++] = '0';
+	ft_strjcpy(ran->buf, s, &ran->j);
+}
+
 char    *ft_print_p(var *s)
 {
 	char*   str;
 	
+	num = va_arg(*vl, unsigned long);
+	if (num == 0)
+	{
+		if (s->minus == 0 && s->precision == 0)
+		{
+			while (s->width-- - 3 > 0)
+				str[i++] = ' ';
+			str[i++] = '0';
+			str[i++] = 'x';
+			str[i++] = '0';
+		}
+		else if (s->precision == -1)
+		{
+			while (s->width-- - 2 > 0)
+				str[i++] = ' ';
+			str[i++] = '0';
+			str[i++] = 'x';
+		}
+		else
+		{
+			str[i++] = '0';
+			str[i++] = 'x';
+			str[i++] = '0';
+			while (s->precision-- > 1)
+				str[i++] = '0';
+			while (s->width-- - 3 > 0)
+				str[i++] = ' ';
+		}
+	}
+	else
+	{
+		s->i = s->j;
+		while (num != 0)
+		{
+			remainder = num % 16;
+			if (remainder < 10)
+				s[s->j++] = 48 + remainder;
+			else
+				s[s->j++] = 55 + 32 + remainder;
+			num = num / 16;
+		}
+		s->j--;
+		while (s->i < s->j)
+		{
+			remainder = s[s->i];
+			s[s->i++] = s[s->j];
+			s[s->j--] = remainder;
+		}
+		ran->strlen = ft_strlen(s) + 2;
+		if (s->minus == 0)
+		{
+			if (s->width < ran->strlen)
+				ft_supzerox(s, ran);
+			else
+			{
+				while (s->width-- > ran->strlen)
+					str[i++] = ' ';
+				ft_supzerox(s, ran);
+			}
+		}
+		else
+		{
+			ft_supzerox(s, ran);
+			while (s->width-- > ran->strlen)
+				str[i++] = ' ';
+		}
+	}
 	return (str);
 }
+
 /*
 void	ft_supoctalsminus(t_spec *s, t_ran *ran)
 {
 	if (s->minus == 0 && s->precision == 0)
 	{
 		while (s->width-- - 3 > 0)
-			ran->buf[ran->j++] = ' ';
-		ran->buf[ran->j++] = '0';
-		ran->buf[ran->j++] = 'x';
-		ran->buf[ran->j++] = '0';
+			str[i++] = ' ';
+		str[i++] = '0';
+		str[i++] = 'x';
+		str[i++] = '0';
 	}
 	else if (s->precision == -1)
 	{
 		while (s->width-- - 2 > 0)
-			ran->buf[ran->j++] = ' ';
-		ran->buf[ran->j++] = '0';
-		ran->buf[ran->j++] = 'x';
+			str[i++] = ' ';
+		str[i++] = '0';
+		str[i++] = 'x';
 	}
 	else
 	{
-		ran->buf[ran->j++] = '0';
-		ran->buf[ran->j++] = 'x';
-		ran->buf[ran->j++] = '0';
+		str[i++] = '0';
+		str[i++] = 'x';
+		str[i++] = '0';
 		while (s->precision-- > 1)
-			ran->buf[ran->j++] = '0';
+			str[i++] = '0';
 		while (s->width-- - 3 > 0)
-			ran->buf[ran->j++] = ' ';
+			str[i++] = ' ';
 	}
 }
 
 void	ft_supzerox(t_spec *s, t_ran *ran)
 {
-	ran->buf[ran->j++] = '0';
-	ran->buf[ran->j++] = 'x';
+	str[i++] = '0';
+	str[i++] = 'x';
 	if (s->precision > s->width)
-		while (s->precision-- > ft_strlen(s->buf))
-			ran->buf[ran->j++] = '0';
+		while (s->precision-- > ft_strlen(s))
+			str[i++] = '0';
 	ft_strjcpy(ran->buf, s, &ran->j);
 }
 
@@ -56,19 +134,19 @@ void	ft_adrxuli(t_spec *s, t_ran *ran, unsigned long long int num,
 	{
 		remainder = num % 16;
 		if (remainder < 10)
-			s->buf[s->j++] = 48 + remainder;
+			s[s->j++] = 48 + remainder;
 		else
-			s->buf[s->j++] = 55 + sm + remainder;
+			s[s->j++] = 55 + sm + remainder;
 		num = num / 16;
 	}
 	s->j--;
 	while (s->i < s->j)
 	{
-		remainder = s->buf[s->i];
-		s->buf[s->i++] = s->buf[s->j];
-		s->buf[s->j--] = remainder;
+		remainder = s[s->i];
+		s[s->i++] = s[s->j];
+		s[s->j--] = remainder;
 	}
-	ran->strlen = ft_strlen(s->buf) + 2;
+	ran->strlen = ft_strlen(s) + 2;
 }
 
 void	ft_supadr1(t_spec *s, t_ran *ran, unsigned long long int num)
@@ -81,7 +159,7 @@ void	ft_supadr1(t_spec *s, t_ran *ran, unsigned long long int num)
 		else
 		{
 			while (s->width-- > ran->strlen)
-				ran->buf[ran->j++] = ' ';
+				str[i++] = ' ';
 			ft_supzerox(s, ran);
 		}
 	}
@@ -89,7 +167,7 @@ void	ft_supadr1(t_spec *s, t_ran *ran, unsigned long long int num)
 	{
 		ft_supzerox(s, ran);
 		while (s->width-- > ran->strlen)
-			ran->buf[ran->j++] = ' ';
+			str[i++] = ' ';
 	}
 }
 
