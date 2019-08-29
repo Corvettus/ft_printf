e6dcc9ecfd38a651f1eadd9ed7539054bdda4204
Thomas Lynesse hightower <tlynesse@il-k4.21-school.ru>
2019-08-29 16:49:57 +0300

    toa

--- ft_printf.h
+++ ft_printf.h
@@ -12,13 +12,14 @@
 
 typedef struct var
 {
-	char*                   origin_data;
-	char					flag;
-	int						width;
-	int                   	precision;
-	char                    type;
-	char*                   data;
-	struct var*             next;
+	char*       origin_data;
+	char        flag;
+	int         width;
+	int         precision;
+	char        type;
+	int         arg_sign;
+	char*       data;
+	struct var* next;
 } var;
 
 typedef struct result_list
@@ -27,10 +28,10 @@ typedef struct result_list
 	struct result_list*		next;
 } result_list;
 
-int		ft_printf(const char *format, ...);
-char	*ft_print_c(var *s);
-char	*ft_print_s(var *s);
-char	*ft_print_p(var *s);
+int     ft_printf(const char *format, ...);
+char    *ft_print_c(var *s);
+char    *ft_print_s(var *s);
+char    *ft_print_p(var *s);
 char	*ft_print_pc(var *s);
 char	*ft_print_d(var *s);
 char	*ft_print_o(var *s);
