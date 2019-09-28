92a14a8d0fb330bb487a775934ce80026c65d40f
Thomas Lynesse hightower <tlynesse@ox-l2.21-school.ru>
2019-09-28 19:11:36 +0300

    govnocode

--- ft_print_o.c
+++ ft_print_o.c
@@ -6,7 +6,7 @@
 /*   By: tlynesse <tlynesse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/03 16:39:28 by rpoetess          #+#    #+#             */
-/*   Updated: 2019/09/21 05:58:57 by tlynesse         ###   ########.fr       */
+/*   Updated: 2019/09/28 19:11:13 by tlynesse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,35 @@
 
 char	*ft_print_o(var *tmp)
 {
-	char	*res;
-
 	if (tmp->width < tmp->precision)
 		tmp->width = tmp->precision;
-	if (tmp->flag == '?' && (int)ft_strlen(tmp->data) < tmp->precision)
-		tmp->flag = '0';
-	while (tmp->flag == '0' && (int)ft_strlen(tmp->data) < tmp->width)
-		tmp->data = ft_strjoin_left("0", tmp->data);
-	if (tmp->flag2 == '#' && tmp->flag != '0')
-		tmp->data = ft_strjoin_left("0", tmp->data);
-	if (tmp->flag == '-')
+	if (tmp->flag == '?' || tmp->flag == '0' || tmp->flag == '-')
 		while ((int)ft_strlen(tmp->data) < tmp->precision)
 			tmp->data = ft_strjoin_left("0", tmp->data);
-	tmp->precision = 0;
-	res = ft_print_s(tmp);
-	return (res);
+//	if (tmp->flag == '0')
+//		while ((int)ft_strlen(tmp->data) < tmp->width)
+//			tmp->data = ft_strjoin_left("0", tmp->data);
+	if (tmp->flag2 == '#' && tmp->flag != '0')
+		tmp->data = ft_strjoin_left("0", tmp->data);
+	
+	if (tmp->width > (int)ft_strlen(tmp->data))
+	{
+		if (tmp->flag != '-')
+			tmp->data = ft_fil_whitespaces(tmp, tmp->width, (int)ft_strlen(tmp->data));
+		else if (tmp->flag == '-')
+			tmp->data = ft_end_whitespaces(tmp, tmp->width, (int)ft_strlen(tmp->data));
+	}
+	
+	return (tmp->data);
 }
+//if (tmp->precision)
+		//tmp->data = ft_crop_str(tmp->data, (int)ft_strlen(tmp->data), tmp->precision);
+/*
+if (tmp->width && tmp->width > (int)ft_strlen(tmp->data))
+	{
+		if (tmp->flag != '-')
+			tmp->data = ft_fil_whitespaces(tmp, wdth, len);
+		else if (tmp->flag == '-')
+			tmp->data = ft_end_whitespaces(tmp, wdth, len);
+	}
+*/
