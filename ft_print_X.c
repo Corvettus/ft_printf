7c3d05971878fed13ec562b1e44b1ff6fb7ec80d
Redwyn Poetess <rpoetess@mi-v5.21-school.ru>
2019-09-29 14:03:09 +0300

    x-end

--- ft_print_X.c
+++ ft_print_X.c
@@ -23,13 +23,6 @@ void	ft_ox(var *tmp)
 
 char	*ft_print_x(var *tmp)
 {
-	/*
-	ft_putchar('|');
-	ft_putchar(tmp->flag);
-	ft_putchar(tmp->flag_1);
-	ft_putchar(tmp->flag2);
-	ft_putchar('|');
-	*/
 	int	flag_zero;
 
 	flag_zero = 0;
@@ -45,47 +38,39 @@ char	*ft_print_x(var *tmp)
 				while (tmp->width > (int)ft_strlen(tmp->data))
 					tmp->data = ft_strjoin("0", tmp->data);
 			}
+			if (tmp->flag == '0' && tmp->flag2 == '#' && tmp->flag_1 == '?')
+			{
+				tmp->width -= 2;
+				while (tmp->width > (int)ft_strlen(tmp->data))
+					tmp->data = ft_strjoin("0", tmp->data);
+			}
 		}
 		if (tmp->precision_flag == 1)
 		{
 			if (tmp->flag == '?' && tmp->flag2 == '#')
-			{
 				if (tmp->flag_1 == '-' || tmp->flag_1 == '?' || tmp->flag_1 == '0')
-				{
-					//if (tmp->precision > tmp->width)
-						while ((int)ft_strlen(tmp->data) < tmp->precision)
-							tmp->data = ft_strjoin("0", tmp->data);		
-				}
-			}
+					while ((int)ft_strlen(tmp->data) < tmp->precision)
+						tmp->data = ft_strjoin("0", tmp->data);		
 			if (tmp->flag == '-' && tmp->flag2 == '#')
+				if (tmp->flag_1 == '0' || tmp->flag_1 == '?')
+					while (tmp->precision > (int)ft_strlen(tmp->data))
+						tmp->data = ft_strjoin("0", tmp->data);
+			if (tmp->flag == '0' && tmp->flag2 == '#' && tmp->flag_1 == '?')
 			{
-				if (tmp->flag_1 == '0')
-				{
-					while (tmp->precision > (int)ft_strlen(tmp->data))
-						tmp->data = ft_strjoin("0", tmp->data);
-				}
-				if (tmp->flag_1 == '?')
-				{
-					while (tmp->precision > (int)ft_strlen(tmp->data))
-						tmp->data = ft_strjoin("0", tmp->data);
-				}
+				tmp->flag2 = '?';
+				while ((int)ft_strlen(tmp->data) < tmp->precision)
+					tmp->data = ft_strjoin("0", tmp->data);
+				ft_ox(tmp);
 			}
-			/*if (tmp->flag == 0 && tmp->flag2 == '#' && tmp->flag_1 == '?')
-				if (tmp->precision < tmp->width)
-					tmp->flag = '?';*/
 		}
-		if (tmp->flag == '?')
-		{
-			if (tmp->flag2 == '#')
+		if (tmp->flag == '?' && tmp->flag2 == '#')
+			ft_ox(tmp);
+		if (tmp->flag == '-' && tmp->flag2 == '#')
+			if (tmp->flag_1 == '?' || tmp->flag_1 == '0')
 				ft_ox(tmp);
-		}
-		if (tmp->flag == '-')
-		{
-			if (tmp->flag2 == '#' && tmp->flag_1 == '?')
+		if (tmp->flag == '0' && tmp->flag2 == '#' && tmp->flag_1 == '?')
+			if (tmp->precision_flag == 0)
 				ft_ox(tmp);
-			if (tmp->flag2 == '#' && tmp->flag_1 == '0')
-				ft_ox(tmp);
-		}
 	}
 	if (tmp->precision_flag == 0 && (int)ft_strlen(tmp->data) == 0)
 	{
@@ -95,25 +80,15 @@ char	*ft_print_x(var *tmp)
 				tmp->data = ft_strjoin("0", tmp->data);
 	}
 	if (tmp->precision_flag == 1 && (int)ft_strlen(tmp->data) == 0)
-	{
 		if (tmp->flag == '0' && tmp->flag2 == '#' && tmp->flag_1 == '?')
 		{
 			tmp->data = "0";
 			tmp->flag = '?';
 			tmp->flag2 = '?'; 
 		}
-	}
-	
 	tmp->data = ft_print_d(tmp);
-
-	if (flag_zero == 0)
-	{
-		if (tmp->flag == '0')
-		{
-			if (tmp->precision >= (int)ft_strlen(tmp->data))
-				if (tmp->flag2 == '#' && tmp->flag_1 == '?')
-					ft_ox(tmp);
-		}
-	}
+	if (tmp->precision >= (int)ft_strlen(tmp->data) && flag_zero == 0)
+		if (tmp->flag == '0' && tmp->flag2 == '#' && tmp->flag_1 == '?')
+			ft_ox(tmp);
 	return (tmp->data);
 }
